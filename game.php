f02b1ea6a02b731d938d4ef515de0d95561d005f
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-05-29 08:25:02 +0200

    start btn desapear on start

--- game.php
+++ game.php
@@ -8,15 +8,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     switch($_POST){
         case isset($_POST['hit']):
-        var_dump($_SESSION['game'][0]);
         $player->hit();
-        var_dump($_SESSION['game'][0]);
-
         header("Location: home.php");
-        
-        // var_dump($_POST);
         break;
         case isset($_POST['stand']):
+        $player->stand();
+        header("Location: home.php");
         var_dump($_POST);
         break;
         case isset($_POST['surrender']):
--- home.php
+++ home.php
@@ -4,10 +4,18 @@ session_start();
 if(isset($_POST['start'])){
     $player = new Blackjack();
     $dealer = new Blackjack();
-    $_SESSION['game'] = [$player, $dealer];
-    var_dump($_SESSION['game']);
-}
-
+    $game = "started";
+    $_SESSION['game'] = [$player, $dealer, $game];
+    }
+if(isset($_SESSION['game'])){
+       $resoults = "";
+       $player = $_SESSION['game'][0];
+       $dealer = $_SESSION['game'][1];
+       if($player->score>21){
+        $resoults = "lose";
+        echo "<p>You lose</p>";
+       }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -24,63 +32,54 @@ if(isset($_POST['start'])){
 </head>
 
 <body>
-    <form action="home.php" method="post">
+    <form action="home.php" method="post" class="d-<?php if(isset($_POST['start']) || isset($_SESSION['game'])){echo "none";}else{echo "block";} ?>">
         <button type="submit" name="start" class="btn btn-outline-success d-block mx-auto mt-5 start">START</button>
     </form>
-
     <div
-        class="container mt-5 bg-dark rounded shadow py-5 px-5 centered d-<?php if(isset($_SESSION['game'])){echo "block";}else{echo "none";} ?>">
+        class="container mt-5 bg-dark rounded shadow py-5 px-5 mx-auto d-<?php if(isset($_SESSION['game'])){echo "block";}else{echo "none";} ?>">
+       <div class="row text-center">
+           <div class="col">
+               <p class="bg-light py-2 px-2 play text-dark">Player</p>
+               <div class="bg-light play py-5 px-5 text-dark"><?php 
+                    if(isset($_SESSION['game'])){
+                       // var_dump($_SESSION['game'][0]);
+                       echo $player->score;
+                    }
+                    ?></div>
+           </div>
+           <div class="col">
+               <p class="bg-light py-2 px-2 play text-dark">Dealer</p>
+               <div class="bg-light play py-5 px-5 text-dark"><?php 
+                    if(isset($_SESSION['game'])){
+                       echo $dealer->score;
+                    }
+                    ?></div>
+           </div>
+       </div>
+    </div>
+    <div class="container mt-5 bg-dark rounded shadow py-5 px-5  d-<?php if(isset($_SESSION['game'])){echo "block";}elseif(isset($_SESSION['game']) && $resoults == "lose"){echo "none";}else{echo "none";} ?>">
         <div class="row">
             <div class="col">
                 <form action="game.php" method="post">
                     <button type="submit" name="hit"
-                        class="w-100 p-5 bg-light btn btn-outline-secondary play">HIT</button>
+                        class="w-100 bg-light btn btn-outline-secondary play">HIT</button>
                 </form>
             </div>
             <div class="col">
                 <form action="game.php" method="post">
                     <button type="submit" name="stand"
-                        class="w-100 p-5 bg-light btn btn-outline-secondary play">STAND</button>
+                        class="w-100 bg-light btn btn-outline-secondary play">STAND</button>
                 </form>
             </div>
             <div class="col">
                 <form action="game.php" method="post">
                     <button type="submit" name="surrender"
-                        class="w-100 p-5 bg-light btn btn-outline-secondary play">SURRENDER</button>
+                        class="w-100 bg-light btn btn-outline-secondary play">SURRENDER</button>
                 </form>
             </div>
         </div>
     </div>
-    <div class="results w-25 mx-auto d-<?php if(isset($_POST['start'])||isset($_SESSION['game'])){echo "block";}else{echo "none";} ?>">
-        <a href="clearsession.php">clear session</a>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Player</th>
-                    <th scope="col">Dealer</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><?php 
-                    if(isset($_SESSION['game'])){
-                       // var_dump($_SESSION['game'][0]);
-                       echo $_SESSION['game'][0]->score;
-                    }
-                    ?></td>
-                    <td><?php 
-                    if(isset($_SESSION['game'])){
-                       // var_dump($_SESSION['game'][0]);
-                       echo $_SESSION['game'][1]->score;
-                    }
-                    ?></td>
-                </tr>
-            </tbody>
-        </table>
-        <?php
-   
-    ?>
-    </div>
+    <a href="clearsession.php">clear session</a>
 </body>
 
 </html>
